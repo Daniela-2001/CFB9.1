85420dbf22a691f86795c44e879e8bc0706daf32
Daniela Duarte <danielatavaresd@gmail.com>
2024-01-17 10:36:01 +0000

    CFB9.1

---
+++ aula09/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace aula09
+{
+    internal class Program
+    {
+        static void Main(){
+
+            int num = 10;
+
+            num = num << 2; //multiplica por 2
+
+            num = num >> 1; //divide por 2
+
+            Console.WriteLine(num);
+
+        }
+    }
+}
